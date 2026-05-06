d95ab2f39817e25d078121e50b0b7c63864cfd5d
Rapeephat-art <rapeephat.ch@rmuti.ac.th>
2026-05-07 00:21:54 +0700

    fix aiven ssl

--- db.js
+++ db.js
@@ -1,18 +1,22 @@
 // backend/db.js
-const mysql = require('mysql2/promise');
-
-// ❗ ไม่ต้อง require dotenv ตรงนี้
-// ให้โหลดที่ server.js ที่เดียวพอ
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  charset: 'utf8mb4'
+
+  charset: "utf8mb4",
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
